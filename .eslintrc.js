397a2a53e84ea6db23e5530d2f393c1705bb98c5
damatie <edafemaxwell@gmail.com>
2024-07-03 15:22:43 +0100

    fix:update favicon

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "prettier",
+  ],
 
   overrides: [],
   parserOptions: {
